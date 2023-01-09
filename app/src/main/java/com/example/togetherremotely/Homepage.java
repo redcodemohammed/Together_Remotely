4262f21e98d1ee4b21609b571cf5c3a51e08d78a
redcodemohammed <redcodemohammed@gmail.com>
2023-01-09 20:07:53 +0300

    feat: added Basic stat display logic

--- app/src/main/java/com/example/togetherremotely/Homepage.java
+++ app/src/main/java/com/example/togetherremotely/Homepage.java
@@ -1,14 +1,111 @@
 package com.example.togetherremotely;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class Homepage extends AppCompatActivity {
+    private String hostname;
+    private String ipAddress;
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+
+    private TextView upTimeTextView;
+    private TextView tempTextView;
+
+    private ProgressBar ramProgress;
+    private TextView ramUsageText;
+
+    private ProgressBar storageProgress;
+    private TextView storageUsageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        upTimeTextView = findViewById(R.id.upTimeTextView);
+        tempTextView = findViewById(R.id.tempTextView);
+
+        ramProgress = findViewById(R.id.ramProgress);
+        ramUsageText = findViewById(R.id.ramUsageText);
+
+        storageProgress = findViewById(R.id.storageProgress);
+        storageUsageText = findViewById(R.id.storageUsageText);
+
+        Intent intent = getIntent();
+        hostname = intent.getStringExtra("hostname");
+        ipAddress = intent.getStringExtra("ipAddress");
+
+        // get basic stats from the api
+        getBasicStats();
+    }
+
+    private void getBasicStats(){
+        Request request = new Request.Builder().url("http://" + ipAddress + ":3000/stats/basic").build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "App is not running on " + ipAddress, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    ResponseBody responseBody = response.body();
+                    String responseString = responseBody.string();
+                    try {
+                        JSONObject jsonObject = new JSONObject(responseString);
+
+                        // UP time
+                        float upTime = jsonObject.getInt("upTime");
+                        upTimeTextView.setText("Up Time: " + upTime + "min");
+
+                        // CPU temperature
+                        float temp = jsonObject.getInt("cpuTemperature");
+                        tempTextView.setText("CPU temp: " + temp + "C°");
+
+                        // RAM usage
+                        JSONObject ram = jsonObject.getJSONObject("ram");
+                        double ramUsage =  ((ram.getDouble("used") / ram.getDouble("total")) * 100);
+                        ramProgress.setProgress((int) ramUsage);
+                        ramUsageText.setText("RAM Usage: " + (int) ramUsage + "%");
+
+                        // Storage usage
+                        JSONObject storage = jsonObject.getJSONObject("storage");
+                        double storageUsage =  (((storage.getDouble("total") - storage.getDouble("available")) / storage.getDouble("total")) * 100);
+                        storageProgress.setProgress((int) storageUsage);
+                        storageUsageText.setText("Storage Usage: " + (int) storageUsage + "%");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/togetherremotely/MainActivity.java
+++ app/src/main/java/com/example/togetherremotely/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.togetherremotely;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
     private EditText ipEditText;
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +36,47 @@ public class MainActivity extends AppCompatActivity {
         ipEditText = findViewById(R.id.edittext_ip);
     }
 
-    public void addConnection(View view){
-        Intent intent = new Intent(this, Homepage.class);
-        startActivity(intent);
+    public void addConnection(View view) {
+        String ipAddress = ipEditText.getText().toString();
+        Request request = new Request.Builder().url("http://" + ipAddress + ":3000/ping").build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+              runOnUiThread(new Runnable() {
+                  public void run() {
+                      Toast.makeText(getApplicationContext(), "App is not running on " + ipAddress, Toast.LENGTH_LONG).show();
+                  }
+              });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    ResponseBody responseBody = response.body();
+                    String responseString = responseBody.string();
+                    try {
+                        JSONObject jsonObject = new JSONObject(responseString);
+                        String hostname = jsonObject.getString("hostname");
+
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+
+                                Toast.makeText(getApplicationContext(), "Connected to " + hostname, Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(getApplicationContext(), Homepage.class);
+
+                                intent.putExtra("ipAddress", ipAddress);
+                                intent.putExtra("hostname", hostname);
+
+                                startActivity(intent);
+                            }
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
     }
 }
